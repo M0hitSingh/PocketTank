1bec2815f49e347c5649605c2733bb9a8a2e7c61
golchi <golchigeek@gmail.com>
2021-10-08 18:05:20 +0530

    adding projectile logic

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,27 @@
-var a = null;
-function projectile(a){
-    
+var a = null
+const pi = Math.PI;
+var pow = 30;
+var angl =45;
+var rng;
+var dir =1;
+var i=0 , y=0;
+angle(angl);
+function angle(angl){
+    radian = angl*(pi/180);
+    range(pow , radian);
+}
+function range(pow , radian ){
+    rng = (pow*pow) * (Math.sin(2*radian))/10;
+    console.log(rng);
+    projectile(pow ,angl ,rng ,dir);
+}
+function projectile(pow , angl , rng , dir){
+    setTimeout(function(){
+        console.log(y)
+        y = (Math.tan(radian))*i - ( 10*(i*i) )/ (  (2*(pow*(Math.cos(radian)) ) )* (pow*(Math.cos(radian)))   );
+        i++;
+        if(i <= rng){
+            projectile(pow , angl , rng , dir);
+        }
+    },1)
 }
