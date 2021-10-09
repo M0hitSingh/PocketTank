92e1cd5fd3250f5c0aaf6f9db3a68d47ed5c7af9
Sandeep Gupta <guptasg300@gmail.com>
2021-10-09 10:57:42 +0530

    key and health bar added

--- javascript/index2.js
+++ javascript/index2.js
@@ -1,20 +1,57 @@
+var count = 0;
+
 function start() {
-    document.body.addEventListener('keydown', function(event)
-    {
-const key = event.key;
-switch (key) {
-case "ArrowLeft":
-str = 'Left';
-break;
-case "ArrowRight":
-str = 'Right';
-break;
-case "ArrowUp":
-str = 'Up';
-break;
-case "ArrowDown":
-str = 'Down';
-break;
+    var angle = document.getElementById("angle");
+    var power = document.getElementById("power");
+
+
+    window.addEventListener("keydown",function(event){
+        if(event.key==="q"||event.key==="Q"){
+            if(parseInt(angle.innerHTML)>1){
+                angle.innerHTML=parseInt(angle.innerHTML)-1;
+
+            }
+         }
+        }
+    )
+    window.addEventListener("keydown",function(event){
+        if(event.key==="e"||event.key==="E"){
+            if(parseInt(angle.innerHTML)<89){
+                angle.innerHTML=parseInt(angle.innerHTML)+ 1;       
+            }
+         }
+        }
+    )
+
+window.addEventListener("keydown",function(event){
+    if(event.key==="a"||event.key==="A"){
+        if(parseInt(power.innerHTML)>1){
+            power.innerHTML=parseInt(power.innerHTML)-1;   
+        }
+     }
+    }
+)
+window.addEventListener("keydown",function(event){
+    if(event.key==="d"||event.key==="D"){
+        if(parseInt(power.innerHTML)<99){
+            power.innerHTML=parseInt(power.innerHTML)+1;
+        }
+             }
+    }
+)
+
+//we have to disable enter button
+    window.addEventListener("keydown",function(event){
+        if(event.key==="Enter"){
+            if(count ==0){
+            count++;
+            fire(parseInt(power.innerHTML),parseInt(angle.innerHTML),1);
+            }
+    
+        }
+    }
+    )
 }
-});
-}
+
+
+
